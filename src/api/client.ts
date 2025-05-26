183a97e76c8d904737168a0e3b0bb67a6689f3be
Hector Lozano <hectorlg87@gmail.com>
2025-05-26 03:02:47 +0200

    feat: implement user storage and API endpoints for authentication and styles

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import type { CreatedUser, CreatedUserResponse } from "../utils/auth";
+import { USER_ENDPOINTS } from "../utils/endpoints";
 
 export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL_API,
@@ -10,11 +11,12 @@ export const apiClient = axios.create({
 
 export const createUser = async (credentials: CreatedUser) => {
   try {
-    const response = await apiClient.post<CreatedUserResponse>("/api/auth/signup", credentials);
-    return response.data
+    const response = await apiClient.post<CreatedUserResponse>(
+      USER_ENDPOINTS.SIGNUP,
+      credentials,
+    );
+    return response.data;
   } catch (error) {
     console.error("Error durante la creacion de usuario: ", error);
   }
 };
-
-
